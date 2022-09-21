985ae4e0ef9d1eb41e2988d85e25f33c8b2f10e2
davigledson <davigledson@outlook.com>
2022-09-21 04:33:02 -0300

    if e else

---
+++ aula011/ex010.js
@@ -0,0 +1,9 @@
+//Condições (Parte 1) - Curso JavaScript #11
+console.log('O console funcionou corretamente')
+var vel = 40
+if(vel >60){ //condição simples!
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log(`A velocidade do seu caro é ${vel}km/h`)
+console.log('Digira com cuidado!')
---
+++ aula011/ex011.js
@@ -0,0 +1,5 @@
+//Condições (Parte 1) - Curso JavaScript #11
+var pais= 'Brasil'
+if (pais =='Brasil'){
+    console.log('Brasileiro!')
+} else{console.log('Estrangeiro!')}
