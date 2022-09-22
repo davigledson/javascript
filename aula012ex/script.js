e121e807b51406e035faece615131c78087208b1
davigledson <davigledson@outlook.com>
2022-09-22 06:12:42 -0300

    Exercícios JavaScript (Parte 2) - Curso JavaScript

--- aula012ex/script.js
+++ aula012ex/script.js
@@ -1,1 +1,27 @@
-alert('Olá')
+function carregar(){
+    var msm =window.document.getElementById('msm')
+    var img = document.getElementById('imagem')
+    var data =new Date()
+    var hora = data.getHours()
+    
+    
+    msm.innerHTML = `Agora são ${hora} horas.`
+    
+    if (hora >= 0 && hora <12){
+        //BOM DIA
+        img.src ='foto_dia.jpg'
+        document.body.style.background ='lightblue'
+        
+    } else if(hora>=12 && hora<18){
+        //BOA TARDE
+        img.src ='foto_tarde.jpg'
+        document.body.style.background ='lightsalmon'
+    } else{
+        //BOA NOITE
+        img.src ='foto_noite.jpg'
+        document.body.style.background ='lightslategray'
+        
+    }
+    
+    
+}
