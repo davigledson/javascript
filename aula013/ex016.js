016082fe2eaa0a62ed3a7a0c1d473cef75ff92f0
davigledson <davigledson@outlook.com>
2022-09-23 03:21:21 -0300

    Create ex016.js

---
+++ aula013/ex016.js
@@ -0,0 +1,17 @@
+//Repetições (Parte 1) - Curso JavaScript #13
+//while = estrutura de repetição com teste no inicio
+console.log('Tudo bem')
+var c = 1
+console.log(` ----- while ----- `)
+while (c<=5){
+    console.log(`${c}° Tudo bem!`)
+    c+=1
+}
+
+console.log(` ----- do while ----- `)
+// do while = estrutura com repetição com teste no final
+var d =1
+do{
+    console.log(`${d}° Olá`)
+    d+=1
+} while(d<=5)
