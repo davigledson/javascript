a270c0842ab5e6e1980ba20c5ea3902f4c559f2e
davigledson <davigledson@outlook.com>
2022-09-25 18:20:20 -0300

    Exercícios JavaScript (Parte 8) - Curso JavaScript

--- aula016ex1/script.js
+++ aula016ex1/script.js
@@ -1,6 +1,7 @@
 
     let num = document.getElementById('num')
     let res = document.getElementById('res')
+    let fim = document.getElementById('fim')
     let valores = []
 
     function isnumero(n){
@@ -24,8 +25,40 @@
 
     function adicionar(){
    if (isnumero(num.value) && !inlista(num.value, valores)){ //!inlista = NÃO EM LISTA
-    window.alert('Tudo ok')
+    valores.push(Number(num.value))
+    res.innerHTML =''
+    for (var pos in valores){
+        
+        res.innerHTML +=`${valores[pos]} Adicionado na lista <br>`
+    }
+    fim.innerHTML =''
    } else{
     window.alert('Valor invalido ou já encontrado na lista')
    }
+   num.value ='' // apaga o numero quando clica = cursor voltar a pisca
+   num.focus() // apaga o numero quando clica = cursor voltar a pisca
+}
+
+function finalizar(){
+    if (valores.length ==0){
+        window.alert('Adicione valores')
+    } else{
+        let total =valores.length
+          //Pode-se usar os métodos do próprio ecmaScript que já nos fornecem o maior ou menor valor, Math.min.apply(Math,vetor) e Math.max.apply(Math,vetor), onde em "vetor" colocamos o nome do vetor criado, sem os [ ]. Estes métodos retornaram o menor ou maior valor.
+        let maior =Math.max.apply(Math,valores)
+        let menor =Math.min.apply(Math,valores)
+        let soma =0
+        let media = 0
+        for (let pos in valores){
+            soma+=valores[pos]
+            
+        }
+        media = soma / valores.length
+        fim.innerHTML =''
+        fim.innerHTML+=`<p>lista de números = ${valores}</p> `
+        fim.innerHTML+= `<p>Ao todo, temos ${total} números cadastrados</p> `
+        fim.innerHTML +=`<p>Maior numero = ${maior}  Menor numero = ${menor} </p>`
+        fim.innerHTML +=`<p>A soma dos valores foi ${soma}</p>`
+        fim.innerHTML += `<p>A media dos valores foi ${media}</p>`
+    }
 }
