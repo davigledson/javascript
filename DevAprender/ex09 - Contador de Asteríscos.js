f6c647d63fc4318a933cec53b65f3dbadca46ec2
davigledson <davigledson@outlook.com>
2022-10-15 23:02:58 -0300

    Create ex09 - Contador de Asteríscos.js

---
+++ DevAprender/ex09 - Contador de Asteríscos.js
@@ -0,0 +1,10 @@
+function exibirastericos(linhas){
+       
+       
+       let  soma=''
+    for(let c=1;c<=linhas;c++){
+       soma+='*' //MELHOR FORMA DE SOMA STRINGS
+    console.log(soma)
+}
+}
+exibirastericos(10)
