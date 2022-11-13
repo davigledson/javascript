c430586adc90b79a3ef00dcb818fbf5fc67d6e92
davigledson <davigledson@outlook.com>
2022-11-12 21:22:31 -0300

    terminando bot simples

--- WebsScraping/index.js
+++ WebsScraping/index.js
@@ -4,14 +4,24 @@ console.log('Robo que acessa sites');
 
 
 async function robo(){
-const browser = await puppeteer.launch({headless: true});
+const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  const moedaBase='dolar';
+  const moedaFinal='real';
+  const qualqueURL = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDIwNzVqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8`;
+  await page.goto(qualqueURL);
+  //await page.screenshot({path: 'example.png'});
 
-  await browser.close();
+  const resultado = await page.evaluate(() => {
+    return document.querySelector(".lWzCpb.a61j6").value;
+  });
+
+  console.log(`O valor de 1 ${moedaBase} em ${moedaFinal} é ${resultado}`)
+
+ // await browser.close();
 
 }
+
 
 robo()
 
